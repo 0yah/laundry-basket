56368aab6704cd6b39f1d1e73113f2251e703276
0yah <highmeme10@gmail.com>
2021-03-22 21:03:51 +0300

    Create elements from JSON respone

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path,include
 from . import views
 
@@ -5,6 +6,11 @@ from . import views
 urlpatterns = [
     path('',views.index,name="home"),
     path('items',views.ItemListView.as_view(),name="pricelist"),
+    path('order',views.order,name="order"),
+    path('jsonitems',views.load_items,name="jsonlist"),
+    url(r'^addcart/(?P<pk>\w{0,50})/$',views.add_cart,name="jsonlist"),
     path('accounts/register', views.register, name="register"),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+r'^addcart/(?P<username>\w{0,50})/$'
--- main/views.py
+++ main/views.py
@@ -1,16 +1,21 @@
+import json
 from django.contrib.auth import login
 from django.shortcuts import redirect, render
 from django.urls.base import reverse
-from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views import generic
 from .models import Item
-from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
-from django.contrib.auth.decorators import login_required,permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
 from .forms import CustomUserCreationForm
+from django.http import JsonResponse
+from django.core import serializers
 
 # Create your views here.
+
+
 def index(request):
-    return render(request,'main/index.html')
+    return render(request, 'main/index.html')
 
 
 def register(request):
@@ -19,20 +24,54 @@ def register(request):
 
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
-        #If the form is valid save the user and redirect them
+        # If the form is valid save the user and redirect them
         if form.is_valid():
             user = form.save()
-            login(request,user)
+            login(request, user)
             return redirect(reverse('home'))
 
-
     context = {
-        'form':form,
+        'form': form,
     }
-    return render(request,'registration/register.html',context)
+    return render(request, 'registration/register.html', context)
+
 
 class ItemListView(generic.ListView):
     model = Item
     context_object_name = "items"
     template_name = 'main/item_list.html'
 
+
+def order(request):
+    return render(request, 'main/order.html')
+
+
+def load_items(request):
+
+    items = serializers.serialize("json", Item.objects.all())
+    data = json.loads(items)
+    return JsonResponse(data, safe=False)
+
+
+def add_cart(request, pk):
+    add_item = Item.objects.get(pk=pk)
+    #request.session['total'] = add_item.price
+
+    test_cart = [
+        {'name': 'Ian', 'price': 9},
+        {'name': 'Jan', 'price': 19},
+        {'name': 'sdkjnkj', 'price': 9}
+    ]
+
+    test_new_data = {
+                {'name': 'Ian', 'price': 9}
+    }
+
+    print(test_new_data in test_cart)
+    #request.session['items'] = add_item.price
+
+    #print()
+
+    items = serializers.serialize("json", Item.objects.all())
+    data = json.loads(items)
+    return JsonResponse(data, safe=False)
